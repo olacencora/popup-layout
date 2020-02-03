d0ce4b7d4ec59ac81211bec555f26088335f6b2a
ola <olacencora@gmail.com>
2020-02-03 21:13:15 +0100

    create second wrapper, add on it display none in js

--- blog-main.js
+++ blog-main.js
@@ -1,3 +1,5 @@
 let btn = document.getElementById("button-to-validate");
 let wrapper1 = document.getElementById("wr-1");
+let wrapper2 = document.getElementById("wr-2");
 console.log(btn);
+wrapper2.style.display = "none";
